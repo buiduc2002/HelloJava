3357131c201e8deefac350dcfad3d15b4e421c57
Bui Duc <buiduc1407@gmail.com>
2023-09-15 16:22:47 +0700

    commit

--- src/Toan.java
+++ src/Toan.java
@@ -4,48 +4,36 @@ public class Toan {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter the number of Chickens: ");
-        int chickens = scanner.nextInt();
-        scanner.nextLine();
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int e = scanner.nextInt();
+        int d = scanner.nextInt();
 
-        System.out.print("Enter the number of Dogs: ");
-        int dogs = scanner.nextInt();
-        scanner.nextLine();
+        for (int x = 0; x <= e; x++) {
+            for (int y = 0; y <= e - x; y++) {
+                int z = e - x - y;
+                // ax + by + cz = d
+                if (z >= 0 && a * x + b * y + c * z == d) {
+                    System.out.println("nghiệm x: " + x + ", nghiệm y: " + y + "nghiệm z: " + z);
+                }
+            }
+        }
+        if (a == 0 && b == 0 && c == 0 && e != 0) {
+            System.out.println("Equation has no integer solution");
 
-        System.out.print("Enter the number of feet: ");
-        int feet = scanner.nextInt();
-        scanner.nextLine();
-
-        System.out.print("Enter the number of horns: ");
-        int horns = scanner.nextInt();
-        scanner.nextLine();
-
-        System.out.println("The number of chickens is: " + chickens);
-        System.out.println("The number of dogs is: " + dogs);
-        System.out.println("The number of feet is: " + feet);
-        System.out.println("The number of horns is: " + horns);
-
-        // Solves for the chickens and horns
-        int f ;
-        int a = chickens;
-        int b = goats - a;
-        int c = goats * f;
-        int d = dogs * f;
-        int e = f;
-    
-        if (a > 0 && f > 0) {
-            f = (b / c) - 1;
-            System.out.println("There are " + a + " chickens and " + c + " horns.");
-        } else if (a > 0) {
-            f = (b / c) - 1;
-            System.out.println("There are " + a + " chickens.");
-        } else {
-            System.out.println("There are " + b + " goats.");
         }
 
-        // Solves for the dogs and horns
-        int f = horns - c;
-        a = chickens;
-        b = dogs - a;
-        c = dogs * f;
-
+        // ax + by = c
+         a = scanner.nextInt();
+         b = scanner.nextInt();
+         c = scanner.nextInt();
+         d =  scanner.nextInt();
+        for (int x = 0; x <= c; x++) {
+            int y = (c - x);
+            if (a * x + b * y == d && y > 0) {
+                System.out.println("Nghiệm x: " + x + ", Nghiệm y: " + y);
+            }
+        }
+    }
+}
